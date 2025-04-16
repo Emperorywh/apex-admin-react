46051487e2f24145a3a80551f557c483f1da54d2
杨文华 <1289978696@qq.com>
2025-04-16 11:21:53 +0800

    feat: 添加Redux和路由配置并更新样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from "./index.module.less";
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './store/counter/counterSlice'
+import { RootState } from "./store/store";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR1111
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const count = useSelector((state: RootState) => state.counter.value)
+
+	const dispatch = useDispatch()
+
+	return (
+		<div className={styles['container']}>
+			<button
+				aria-label="Increment value"
+				onClick={() => dispatch(increment())}
+			>
+				Increment
+			</button>
+			<span>{count}</span>
+			<button
+				aria-label="Decrement value"
+				onClick={() => dispatch(decrement())}
+			>
+				Decrement
+			</button>
+		</div>
+	)
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { store } from './store/store.ts'
+import { Provider } from 'react-redux'
+import {
+	createBrowserRouter,
+	RouterProvider,
+} from "react-router";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
+	</StrictMode>,
 )
